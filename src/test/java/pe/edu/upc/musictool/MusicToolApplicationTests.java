4e8ea8bff68a5e76c9e33e30df04aad3c6168188
XxBrunoRexX <81479212+XxBrunoRexX@users.noreply.github.com>
2024-08-21 18:17:45 -0500

    Creacion de Proyecto en Master

---
+++ src/test/java/pe/edu/upc/musictool/MusicToolApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.musictool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
