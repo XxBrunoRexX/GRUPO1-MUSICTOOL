0d279ad1f899b802e2c3b0523570cab4eb0c5608
RickZ-V <ricmostsal@gmail.com>
2024-09-03 17:48:34 -0500

    Implementación de los metodos Listar, crear, actualizar, eliminar, buscar por usuario y género de la tabla PreRelease_Songs

--- src/main/java/pe/edu/upc/musictool/controllers/PreRelease_SongsController.java
+++ src/main/java/pe/edu/upc/musictool/controllers/PreRelease_SongsController.java
@@ -2,13 +2,12 @@ package pe.edu.upc.musictool.controllers;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pe.edu.upc.musictool.dtos.PreRelease_SongsDTO;
 import pe.edu.upc.musictool.dtos.SurveyDTO;
 import pe.edu.upc.musictool.dtos.UserDTO;
+import pe.edu.upc.musictool.entities.PreRelease_Songs;
+import pe.edu.upc.musictool.entities.Survey;
 import pe.edu.upc.musictool.serviceinterfaces.IPreRelease_SongsService;
 
 import java.util.List;
@@ -26,6 +25,20 @@ public class PreRelease_SongsController {
             return m.map(x, PreRelease_SongsDTO.class);
         }).collect(Collectors.toList());
     }
+    @PostMapping
+    public void insertar(@RequestBody PreRelease_SongsDTO dto){
+        ModelMapper m = new ModelMapper();
+        PreRelease_Songs s = m.map(dto, PreRelease_Songs.class);
+        pS.insertPreRelease(s);
+    }
+    @PatchMapping
+    public void actualizar(PreRelease_SongsDTO dto){
+        ModelMapper m = new ModelMapper();
+        PreRelease_Songs s = m.map(dto, PreRelease_Songs.class);
+        pS.updatetPreRelease(s);
+    }
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable("id")int id){pS.deletePreRelease(id);}
     @GetMapping("/buscargenero")
     public List<PreRelease_SongsDTO> buscarGenero(@RequestParam String genre) {
         return pS.buscarGenero(genre).stream().map(x->{
@@ -33,4 +46,12 @@ public class PreRelease_SongsController {
             return m.map(x,PreRelease_SongsDTO.class);
         }).collect(Collectors.toList());
     }
+    @GetMapping("/buscarnombre")
+    public List<PreRelease_SongsDTO> buscarNombre(@RequestParam String name) {
+        return pS.buscarNombre(name).stream().map(x->{
+            ModelMapper m=new ModelMapper();
+            return m.map(x,PreRelease_SongsDTO.class);
+        }).collect(Collectors.toList());
+    }
+
 }
