bb0c04d8633b45e4ac56e2fcc5abadaa890bde59
Jhampier Eduardo Campos Drago <U201917576@upc.edu.pe>
2024-09-22 22:24:26 -0500

    He realizado mis dos querys - Subscription

--- src/main/java/pe/edu/upc/musictool/repositories/ISuscriptionRepository.java
+++ src/main/java/pe/edu/upc/musictool/repositories/ISuscriptionRepository.java
@@ -1,7 +1,17 @@
 package pe.edu.upc.musictool.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import pe.edu.upc.musictool.entities.Survey;
 import pe.edu.upc.musictool.entities.Suscription;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface ISuscriptionRepository extends JpaRepository<Suscription, Integer> {
+    @Query("select s from Suscription s where s.type_susciption like %:tiposuscripcion%")
+    public List<Suscription> buscartipodesuscripcion(@Param(("tiposuscripcion")) String tiposuscripcion);
+    @Query("select s from Suscription s where s.sub_date between :fechainicio and :fechafin")
+    public List<Suscription> buscarrango(@Param(("fechainicio")) LocalDate fechainicio, @Param(("fechafin")) LocalDate fechafin );
 }
